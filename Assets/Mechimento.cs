c7ef8ded9067e1b3d02d254c8b228a4e884a6236
Yue Lian <evelynmarquesdasilvasenai25@gmail.com>
2026-03-03 10:45:21 -0300

    andar e falha de pular.

--- Assets/Mechimento.cs
+++ Assets/Mechimento.cs
@@ -11,11 +11,30 @@ public class Mechimento : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+    if (Input.GetKey("d"))
+        {
+            transform.Translate(0.05f, 0, 0);
+        }
 
+    if (Input.GetKey("a"))
+        {
+            transform.Translate(-0.05f, 0, 0);
+        }
+
+    if (Input.GetKey("w"))
+        {
+            transform.Translate(0,0.10f, 0);
+        }
+
+        if (!Physics.CheckSphere(transform.position, 1.50f))
+        {
+            transform.Translate(0, -0.1f, 0);
+        }
+;
     }
 
 
-
+}
 
 
 
